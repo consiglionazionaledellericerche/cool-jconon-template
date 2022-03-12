b66fa2f7551b08e6ab228d79b6cae2136c26d56e
marco.spasiano <marco.spasiano@cnr.it>
2022-03-12 11:09:24 +0100

    FIX Protocol application on send change package name

--- src/main/java/it/cnr/si/cool/jconon/service/NuovoEntePrintService.java
+++ src/main/java/it/cnr/si/cool/jconon/service/NuovoEntePrintService.java
@@ -1,8 +1,10 @@
-package it.nuovoente.service;
+package it.cnr.si.cool.jconon.service;
 
 import it.cnr.cool.web.scripts.exception.CMISApplicationException;
-import it.cnr.si.cool.jconon.service.PrintService;
 import org.apache.chemistry.opencmis.client.api.Folder;
+import org.apache.chemistry.opencmis.client.api.Session;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
@@ -11,19 +13,23 @@ import java.io.InputStream;
 @Primary
 @Service
 public class NuovoEntePrintService extends PrintService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(NuovoEntePrintService.class);
 
     @Override
-    public String archiviaRicevutaReportModel(Folder application,
+    public String archiviaRicevutaReportModel(Session cmisSession,
+                                              Folder application,
                                               InputStream is,
                                               String nameRicevutaReportModel,
                                               boolean confermata) throws CMISApplicationException {
-        final String s = super.archiviaRicevutaReportModel(application, is, nameRicevutaReportModel, confermata);
+        LOGGER.info("Start NuovoEntePrintService");
+        final String s = super.archiviaRicevutaReportModel(cmisSession, application, is, nameRicevutaReportModel, confermata);
         if (confermata) {
             /**
              * In questo momento pouò essere invocata la protocollazione della domanda inviata
              * nell' InputStream è presente il pdf della domanda inviata
              */
         }
+        LOGGER.info("End NuovoEntePrintService");
         return s;
     }
 }
